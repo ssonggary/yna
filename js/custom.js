0bcc01a62c4c0c3a457e787ad1a1fbbe8bbfef10
ssonggary <84754325+ssonggary@users.noreply.github.com>
2023-07-18 17:31:05 +0900

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = gsap.utils.toArray(".type");
+let maxWidth = 0;
+
+const getMaxWidth = () => {
+  maxWidth = 0;
+  sections.forEach((section) => {
+    maxWidth += section.offsetWidth;
+  });
+};
+getMaxWidth();
+ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+gsap.to(sections, {
+  xPercent: -70,
+  x: () => `-${maxWidth - window.innerWidth}`,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#wrap",
+    pin: true,
+    scrub: 1,
+    end: () => `+=${maxWidth}`,
+    invalidateOnRefresh: true,
+  },
+});
+// gsap.to(".title-wrap", { autoAlpha: 0.2, top: 0, left: 0, duration: 1 });
+
+const tl1 = gsap.timeline({ repeatDelay: 0.3 });
+tl1.to(".title-wrap", { autoAlpha: 0.2, top: 0, duration: 1 });
+tl1.to(".working-wrap", { autoAlpha: 1, top: "45%", duration: 0.9 });
